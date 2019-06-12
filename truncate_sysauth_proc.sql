ceced2d5c174709daa70b65141481ff7fcc7da9f
bbenzer <berk.benzer@stu.bahcesehir.edu.tr>
2019-06-12 21:20:25 +0300

    Delete truncate_sysauth_proc.sql

--- truncate_sysauth_proc.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE procedure SYS.trunc_sysauth (trunc_sysauth_output OUT VARCHAR2) 
-is
-begin
- execute immediate 'truncate table testing';
- trunc_sysauth_output := 'Success';
-end;
-/
